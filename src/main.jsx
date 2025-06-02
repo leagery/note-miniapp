9f5b97cf75e33ee1e61dbd09ddb27a38f31b4e8f
leagery <banaessine@yandex.com>
2025-06-02 20:46:28 +0200

    Упрощённый main.jsx для диагностики

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+
+function App() {
+  return (
+    <div style={{ padding: 20, color: 'white' }}>
+      <h1>Минимальная проверка работает</h1>
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
